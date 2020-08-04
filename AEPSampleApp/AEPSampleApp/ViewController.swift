cd8b65f0191eeb621578129bc9bac25f47de2d7f
Christopher Hoffmann <choffman@adobe.com>
2020-08-04 10:59:59 -0600

    init project

---
+++ AEPSampleApp/AEPSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AEPSampleApp
+//
+//  Created by Christopher Hoffman on 8/4/20.
+//  Copyright © 2020 Christopher Hoffman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
