7f0e3e3eed263a5064aa5bc8ff517f11cd7426bf
steve benedick <sbenedic@adobe.com>
2020-10-21 15:19:20 -0600

    -updating to new core version and api updates

--- Swift/AEPSampleApp/AppDelegate.swift
+++ Swift/AEPSampleApp/AppDelegate.swift
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // step-init-start
-        MobileCore.setLogLevel(level: .trace)
+        MobileCore.setLogLevel(.trace)
         let appState = application.applicationState;
 
         MobileCore.registerExtensions([Lifecycle.self, Identity.self, Signal.self, Edge.self
