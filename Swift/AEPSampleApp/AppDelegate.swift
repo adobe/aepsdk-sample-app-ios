c563d914075ef49725be3a6e52efb0cda6444c43
jgeng <jgeng@adobe.com>
2020-10-26 10:22:58 -0600

    update pod files

--- Swift/AEPSampleApp/AppDelegate.swift
+++ Swift/AEPSampleApp/AppDelegate.swift
@@ -24,7 +24,10 @@ import AEPAssurance
 //step-extension-start
 import AEPSampleExtensionSwift
 //step-extension-end
+
+//step-edge-start
 import AEPEdge
+//step-edge-start
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -75,14 +78,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        MobileCore.lifecycleStart(additionalContextData: nil)
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        MobileCore.lifecyclePause()
     }
 }
 
